90b76904a1aec77e6953f560b82cea998031efbd
Viipers <vipers12000@gmail.com>
2023-01-06 00:43:42 +0100

    [fix]

--- Module00/ex01/exec.py
+++ Module00/ex01/exec.py
@@ -1,10 +1,8 @@
 import sys
 
 def reverse_and_swap(string):
-  # Reverse the string
   reversed_str = string[::-1]
 
-  # Swap the case of the characters
   swapped_string = ""
   for char in reversed_str:
     if char.islower():
@@ -12,18 +10,12 @@ def reverse_and_swap(string):
     else:
       swapped_string += char.lower()
 
-  # Print the modified string
   print(swapped_string)
 
-# Get the command line arguments
 args = sys.argv[1:]
 
-# If no argument is provided, print an usage message
 if not args:
   print("Usage: python exec.py [string]")
 else:
-  # Merge the arguments into a single string
   string = " ".join(args)
-
-  # Call the reverse_and_swap function
   reverse_and_swap(string)
