f898010e1414bab1efd488e51a8b6c72faa4b718
Viipers <vipers12000@gmail.com>
2023-01-06 01:06:17 +0100

    [+] 2

---
+++ Module00/ex02/whois.py
@@ -0,0 +1,20 @@
+import sys
+
+def check_number(num):
+  if num == 0:
+    print("The number is zero.")
+  elif num % 2 == 0:
+    print("The number is even.")
+  else:
+    print("The number is odd.")
+
+if len(sys.argv) != 2:
+  print("Usage: python whois.py <number>")
+  sys.exit(1)
+
+try:
+  num = int(sys.argv[1])
+except ValueError:
+  print("Error: Please provide exactly one integer argument.")
+  sys.exit(1)
+check_number(num)
