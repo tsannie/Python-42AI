aa8f5d72014c5e3e88f0ca2041abf6db5da7d35a
Viipers <vipers12000@gmail.com>
2023-01-16 10:26:31 +0100

    kata04

--- Module00/ex05/kata02.py
+++ Module00/ex05/kata02.py
@@ -1,4 +1,4 @@
 # Put this at the top of your kata02.py file
 kata = (2019, 9, 25, 3, 30)
 
-print("{:02d}/{:02d}/{} {:02d}:{:02d}".format())
+print("{:02d}/{:02d}/{} {:02d}:{:02d}".format(kata[1], kata[2], kata[0], kata[3], kata[4]))
---
+++ Module00/ex05/kata04.py
@@ -0,0 +1,4 @@
+# Put this at the top of your kata04.py file
+kata = (0, 4, 132.42222, 10000, 12345.67)
+
+print("module_{:02d}, ex_{:02d} : {:.2f}, {:.2e}, {:.2e}".format(kata[0], kata[1], kata[2], kata[3], kata[4]))
