6aaa4ae7ef775f8a89f81c0909156236bef6051a
Viipers <vipers12000@gmail.com>
2023-01-16 11:35:57 +0100

    fix recipe

--- Module00/ex06/recipe.py
+++ Module00/ex06/recipe.py
@@ -16,27 +16,29 @@ cookbook= {
   }
 }
 
-# print all recipe names
 def print_recipe_names():
     print("Recipes:")
     for recipe in cookbook:
         print(recipe)
 
-# takes a recipe name and print its details
 def print_recipe(recipe_name):
     print("Recipe for " + recipe_name + ":")
     print("Ingredients list: " + str(cookbook[recipe_name]["ingredients"]))
     print("To be eaten for " + cookbook[recipe_name]["meal"] + ".")
     print("Takes " + str(cookbook[recipe_name]["prep_time"]) + " minutes of cooking.")
 
-# takes a recipe name and delete it.
 def delete_recipe(recipe_name):
     del cookbook[recipe_name]
 
-# add a recipe from user input.
 def add_recipe():
     recipe_name = input("Enter a name: ")
-    ingredients = input("Enter ingredients: ").split()
+    ingredients = []
+    print("Enter ingredients (or nothing to stop):")
+    while True:
+        ingredient = input("- ")
+        if ingredient == "":
+            break
+        ingredients.append(ingredient)
     meal = input("Enter a meal type: ")
     prep_time = input("Enter a preparation time: ")
     cookbook[recipe_name] = {"ingredients": ingredients, "meal": meal, "prep_time": prep_time}
