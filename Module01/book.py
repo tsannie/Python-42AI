dbf9d60241cc85a72a2178984430b929760dfac1
tsannie <tsannie@student.42.fr>
2023-01-24 12:13:52 +0100

    [+] start module01

---
+++ Module01/book.py
@@ -0,0 +1,7 @@
+
+class Book:
+    def __init__(sekf, name, last_name, birthday, recipes_list):
+        self.name = name
+        self.last_name = last_name
+        self.birthday = birthday
+        self.recipes_list = recipes_list
