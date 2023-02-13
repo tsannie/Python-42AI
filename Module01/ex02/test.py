ce63fdb6e9573497df64783df9d7183a72567f7f
tsannie <tsannie@student.42.fr>
2023-02-13 09:00:44 +0100

    [+] start vector

--- Module01/ex02/test.py
+++ Module01/ex02/test.py
@@ -1,7 +1,15 @@
 from vector import Vector
 
-v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
-v2 = v1 * 5
-#v2
-print(v2)
+# Column vector of shape n * 1
+""" v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+v2 = v1
+print(v2) """
 # Expected output:
+# Vector([[0.0], [5.0], [10.0], [15.0]])
+# Row vector of shape 1 * n
+v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+v2 = v1
+print(v2)
+# Expected output
+# Vector([[0.0, 5.0, 10.0, 15.0]])
+
--- Module01/ex02/vector.py
+++ Module01/ex02/vector.py
@@ -1,10 +1,44 @@
+import numpy as np
+
 class Vector:
+    """ Vector class for linear algebra  """
+
     def __init__(self, *args):
-        if len(args) == 0:
-            self.values = (0, 0)
-        else:
-            self.values = args
+        self.values = np.array(args)
+        print(len(args))
+        self.shape = len(args) == 1 and (1, len(args)) or (len(args), 1)
+        print(self.shape)
+
+    def dot(self, v):
+        return np.dot(self.values, v.values)
+
+    def T(self):
+        return np.transpose(self.values)
+
+    def __add__(self, n):
+        ret = np.add(self.values, n)
+        if self.shape[0] == 1:
+            return Vector(ret[0])
+        return Vector(ret)
+
+    def __sub__(self, n):
+        return np.subtract(self.values, n)
+
+    def __mul__(self, n):
+        return np.multiply(self.values, n)
+
+    def __rmul__(self, n):
+        return np.multiply(self.values, n)
+
+    def __truediv__(self, n):
+        return np.divide(self.values, n)
 
     def __str__(self):
-        return str(self.values)
+        ret = "Vector(["
+        for i in range(len(self.values)):
+            ret += "{:.1f}".format(self.values[i][0])
+            if i != len(self.values) - 1:
+                ret += ", "
+        ret += "])"
+        return ret
 
