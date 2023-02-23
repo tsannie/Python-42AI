6193d4f735c935d383181dbb17ce1aef876f8ef1
tsannie <tsannie@student.42.fr>
2023-02-23 16:02:53 +0100

    [+] end numpy creator

--- Module03/ex00/NumPyCreator.py
+++ Module03/ex00/NumPyCreator.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def check_type(obj, dtype):
-    if not isinstance(obj, dtype) or not isinstance(obj[0], (list, tuple)):
+    if not isinstance(obj, dtype):
         return False
     for i in range(len(obj)):
         if not isinstance(obj[i], type(obj[0])) or len(obj[i]) != len(obj[0]):
@@ -25,19 +25,17 @@ class NumPyCreator:
         return None
 
     @staticmethod
-    def from_iterable(itr):
-        return np.array(itr)
+    def from_iterable(itr, dtype=None):
+        return np.array(itr, dtype)
 
     @staticmethod
-    def from_shape(shape, value=0):
-        return np.full(shape, value)
+    def from_shape(shape, value=0, dtype=None):
+        return np.full(shape, value, dtype)
 
     @staticmethod
-    def random(shape):
-        return np.random.random(shape)
+    def random(shape, dtype=None):
+        return np.full(shape, np.random.random_sample(shape), dtype)
 
     @staticmethod
     def identity(n):
         return np.identity(n)
-
-a = NumPyCreator().from_list([[1,2,3],['a','b','c'],[6,4,7]])
--- Module03/ex00/test.py
+++ Module03/ex00/test.py
@@ -14,7 +14,7 @@ print(a)
 #None
 a = npc.from_list([[1,2,3],['a','b','c'],[6,4,7]])
 print(a)
-print(a.dtype)
+print("dtype= ", a.dtype)
 # Output :
 #array([['1','2','3'],
 #['a','b','c'],
@@ -25,23 +25,27 @@ npc.from_list(((1,2),(3,4)))
 print()
 
 print("Testing from_tuple:")
-npc.from_tuple(("a", "b", "c"))
+a = npc.from_tuple(("a", "b", "c"))
+print(a)
 # Output :
 #array(['a', 'b', 'c'])
-npc.from_tuple(["a", "b", "c"])
+a = npc.from_tuple(["a", "b", "c"])
+print(a)
 # Output :
 #None
 print()
 
 print("Testing from_iterable:")
-npc.from_iterable(range(5))
+a = npc.from_iterable(range(5))
+print(a)
 # Output :
 #array([0, 1, 2, 3, 4])
 print()
 
 print("Testing from_shape:")
 shape=(3,5)
-npc.from_shape(shape)
+a = npc.from_shape(shape)
+print(a)
 # Output :
 #array([[0, 0, 0, 0, 0],
 #[0, 0, 0, 0, 0],
@@ -49,7 +53,8 @@ npc.from_shape(shape)
 print()
 
 print("Testing random:")
-npc.random(shape)
+a = npc.random(shape)
+print(a)
 # Output :
 #array([[0.57055863, 0.23519999, 0.56209311, 0.79231567, 0.213768 ],
 #[0.39608366, 0.18632147, 0.80054602, 0.44905766, 0.81313615],
@@ -57,7 +62,8 @@ npc.random(shape)
 print()
 
 print("Testing identity:")
-npc.identity(4)
+a = npc.identity(4)
+print(a)
 # Output :
 #array([[1., 0., 0., 0.],
 #[0., 1., 0., 0.],
