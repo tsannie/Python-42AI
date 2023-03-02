f83924e34a4aff091dac1300d36d562b4e30633a
tsannie <tsannie@student.42.fr>
2023-03-02 14:23:21 +0100

    [+] first version of kmeans

--- Module03/ex04/Kmeans.py
+++ Module03/ex04/Kmeans.py
@@ -2,9 +2,10 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from csvreader import CsvReader as csv
+from mpl_toolkits.mplot3d import Axes3D
 
 class KmeansClustering:
-    def __init__(self, max_iter=20, ncentroid=5):
+    def __init__(self, max_iter=20, ncentroid=4):
         if not isinstance(max_iter, int):
             print("Error: max_iter is not an integer.")
             exit()
@@ -20,8 +21,16 @@ class KmeansClustering:
         self.ncentroid = ncentroid # number of centroids
         self.max_iter = max_iter # number of max iterations to update the centroids
         self.centroids = [] # values of the centroids
+        self.last_scores = 0.0 # last score of the centroids
+        self.stable = False # True if the centroids are stable
+
+        #plt
+        fig = plt.figure()
+        self.ax = fig.add_subplot(111, projection='3d')
 
 
+    def distance(self, x, y):
+        return np.square(np.sum((x - y) ** 2))
 
     def fit(self, X):
         """
@@ -43,8 +52,29 @@ class KmeansClustering:
         if self.ncentroid > X.shape[0]:
             print("Error: ncentroid is greater than the number of data points.")
             exit()
-        for i in range(self.ncentroid):
-            self.centroids.append(X[i])
+
+        t = np.random.choice(X.shape[0], self.ncentroid, replace=False)
+        self.centroids = X[t]
+
+        for i in range(self.max_iter):
+            i_min = self.predict(X)
+            for j in range(self.centroids.shape[0]):
+                self.centroids[j] = np.mean(X[i_min == j], axis=0)
+
+            #plt
+            self.ax.clear()
+            self.ax.set_xlabel('X Label')
+            self.ax.set_ylabel('Y Label')
+            self.ax.set_zlabel('Z Label')
+            self.ax.set_title("K-means Clustering")
+            self.ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=i_min)
+            self.ax.scatter(self.centroids[:, 0], self.centroids[:, 1], self.centroids[:, 2], c='red', marker='x')
+            print("Iteration: {}".format(i))
+            plt.pause(1)
+            if self.stable:
+                break
+
+        plt.show()
 
     def predict(self, X):
         """
@@ -59,7 +89,21 @@ class KmeansClustering:
         Raises:
         This function should not raise any Exception.
         """
+        dist = np.zeros((X.shape[0], self.ncentroid))
+        for i in range(self.centroids.shape[0]):
+            for e in range(X.shape[0]):
+                dist[e][i] = self.distance(X[e], self.centroids[i])
+        i_min = np.argmin(dist, axis=1)
 
+        score = 0.0
+        for i in range(dist.shape[0]):
+            score += dist[i][i_min[i]]
+        print("Score: {}".format(score))
+        if score == self.last_scores:
+            self.stable = True
+        self.last_scores = score
+
+        return i_min
 
 if __name__ == "__main__":
 
@@ -67,22 +111,16 @@ if __name__ == "__main__":
     if len(args) != 3 or "filepath" not in args or "ncentroid" not in args or "max_iter" not in args:
         print("Usage: python Kmeans.py  filepath=[path_to_dataset] ncentroid=[ncentroid] max_iter=[max_iter]")
         exit()
-    print(args)
-
 
     with csv(args["filepath"], header=True) as file:
         data = file.getdata()
         data = np.array(data).astype(float)
-
-    print(data)
-
-    plt.scatter(data[:, 1], data[:, 2])
-    plt.xlabel("Height")
-    plt.ylabel("Weight")
-    plt.show()
+    data = data[:, 1:]
 
     kc = KmeansClustering(int(args["max_iter"]), int(args["ncentroid"]))
-    #kc.fit(data)
+    kc.fit(data)
+
+
 
 
 
